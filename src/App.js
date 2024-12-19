784aaa1c4a88d839fc7222b17cec3883f334ff0a
prashant-99-lab <chaudharyprashant@mooglelabs.com>
2024-12-19 12:02:17 +0530

    changes in app.js file

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is Learn React
         </a>
       </header>
     </div>
